29fedba8e27fe2ecf8254eea0d4b5947c85fab56
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-20 18:50:17 -0800

    established connection through mysql

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: employeesDB
+});
