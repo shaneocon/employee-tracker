53fd988a8630b56be9d5f116b4e6c1c8c20c3255
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-20 22:25:15 -0800

    added addEmployee() successfully

--- index.js
+++ index.js
@@ -61,14 +61,21 @@ function menu() {
                 updateRole()
                 break;
             
+            default:
+                connection.end()
+                break;
+            
         }
     }).catch((error) => {
         console.log(error);
     })
 }
 function viewEmployees() {
+    console.log("\nViewing employees\n");
+
     var query = "SELECT * FROM employee";
-    connection.query(query, function (err, data){
+
+    connection.query(query, function (err, data) {
         console.table(data);
         menu();
     })
@@ -76,9 +83,55 @@ function viewEmployees() {
 }
     
 
-function viewDepartments() {}
-function viewRoles() {}
-function addEmployee() {}
+function viewDepartments() {
+    console.log("\nViewing departments\n");
+
+    var query = "SELECT * FROM department";
+
+    connection.query(query, function (err, data) {
+        console.table(data);
+        menu();
+    })
+}
+function viewRoles() {
+    console.log("\nViewing roles\n");
+
+    var query = "SELECT * FROM role";
+
+    connection.query(query, function (err, data) {
+        console.table(data);
+        menu();
+    })
+}
+function addEmployee() {
+    inquirer.prompt([{
+        name: "firstName",
+        type: "input",
+        message: "What is the employee's first name?"
+    },
+    {
+        name: "lastName",
+        type: "input",
+        message: "What is the employee's last name?"
+    },
+    {
+        name: "roleId",
+        type: "number",
+        message: "What is the employee's role ID?"
+    },
+    {
+        name: "managerId",
+        type: "number",
+        message: "What is the manager ID of the new employee?"
+    }
+    ]).then(function(res) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [res.firstName, res.lastName, res.roleId, res.managerId], function(err, data) {
+            if (err) console.log(err);
+            console.table("Successfully Inserted");
+            menu();
+        })
+    })
+}
 function addDepartment() {}
 function addRole() {}
 function updateRole() {}
