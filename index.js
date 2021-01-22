c1d0b03376f2ab8bdafbeb146c69eec9d67e785c
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-21 17:56:34 -0800

    update Role function

--- index.js
+++ index.js
@@ -105,7 +105,14 @@ function viewDepartments() {
 function viewRoles() {
     console.log("\nViewing roles\n");
 
-    var query = "SELECT * FROM role";
+    var query = `SELECT e.id, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`;
 
     connection.query(query, function (err, data) {
         console.table(data);
@@ -180,7 +187,9 @@ function addRole() {
             })
         })
 }
-function updateRole() {}
+function updateRole() {
+    inquirer.prompt({})
+}
 
 // menu() could be used as exit
 
